1dcd26860bab8199545780f215ac120bb032eaa4
Christopher Alcock <christopher.alcock@gmail.com>
2015-09-17 23:14:30 +0100

    basis of omnichord

--- app/app.rb
+++ app/app.rb
@@ -7,8 +7,16 @@ class SimplePiano < Sinatra::Base
   register Sinatra::Flash
   use Rack::MethodOverride
 
+  get '/piano' do
+    erb :piano
+  end
+
   get '/' do
     erb :piano
+  end
+
+  get '/omnichord' do
+    erb :omnichord
   end
 
   run! if app_file == $0
