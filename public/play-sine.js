4730d36a3f6be76427053a88185735b1165d47cc
Christopher Alcock <christopheralcock@Christophers-MacBook-Pro-2.local>
2016-02-27 15:19:36 +0000

    implemented fail on more than 20 notes:

--- public/play-sine.js
+++ public/play-sine.js
@@ -20,18 +20,25 @@ function playSine(delay, pitch, duration) {
 }
 
 var noteArray = [];
+var errorArray = [];
 var unlockMelody = [5, 7, 3, -9, -2];
 
 function logSuccess(){
   console.log("SUCCESS");
+  document.getElementById('picture').innerHTML = '<iframe style="position:fixed;top:0px;left:0px;width:100%;height:100%;z-index:-1;" width="560" height="315" src="https://www.youtube.com/embed/4Vv5CsP1pAg?autoplay=1&showinfo=0&rel=0" frameborder="0" allowfullscreen></iframe>';
+  httpGet("http://ec2-54-229-66-207.eu-west-1.compute.amazonaws.com:4000/");
+}
+
+function logFailure(){
+  console.log("FAILURE");
+  document.getElementById('picture').innerHTML = '<iframe style="position:fixed;top:0px;left:0px;width:100%;height:100%;z-index:-1;" width="560" height="315" src="https://www.youtube.com/embed/v5_3XaaeRXI?autoplay=1&showinfo=0&rel=0&start=51&end=58" frameborder="0" allowfullscreen></iframe>';
   httpGet("http://ec2-54-229-66-207.eu-west-1.compute.amazonaws.com:4000/");
 }
 
 function httpGet(theUrl){
     // document.getElementById('picture').innerHTML = "<img style='position:fixed;top:0px;left:0px;width:100%;height:100%;z-index:-1;' src='http://memesvault.com/wp-content/uploads/Sloth-Goonies-Hey-You-Guys-08.gif'/>";
-    document.getElementById('picture').innerHTML = '<iframe style="position:fixed;top:0px;left:0px;width:100%;height:100%;z-index:-1;" width="560" height="315" src="https://www.youtube.com/embed/4Vv5CsP1pAg?autoplay=1&showinfo=0&rel=0" frameborder="0" allowfullscreen></iframe>';
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.open( "GET", theUrl, false );
     xmlHttp.send( null );
     console.log(xmlHttp.responseText);
     document.getElementById('vehicle-response').innerHTML = xmlHttp.responseText;
@@ -39,12 +46,16 @@ function httpGet(theUrl){
 
 function updateNoteArray(pitch){
   noteArray.push(pitch);
+  errorArray.push(pitch);
   noteArray = noteArray.slice(-5);
   console.log(noteArray);
   console.log(unlockMelody[0]-noteArray[0]);
-  console.log(typeof noteArray);
   if (noteArray.toString() == unlockMelody.toString()){
     logSuccess();
+  };
+  console.log(errorArray.length)
+  if (errorArray.length > 20){
+    logFailure();
   };
 
 }
