0f134e06695531860115af2908f394bee143b405
Christopher Alcock <christopher.alcock@gmail.com>
2016-03-01 14:20:29 +0000

    Update play-sine.js

--- public/play-sine.js
+++ public/play-sine.js
@@ -25,6 +25,7 @@ var unlockMelody = [0, 2, -2, -14, -7];
 var closeEncounters = [0, 2, -2, -14, -7];
 var jaws = [0,1,0,1,0];
 var imperial = [0,-3,-7,0,-3];
+var goldfinger = [0,7,2,4,-4];
 
 function logSuccess(){
   console.log("SUCCESS");
@@ -40,6 +41,13 @@ function launchImperial(){
   console.log("IMMA GONNA BLOW UP THE SPACE!");
   document.getElementById('picture').innerHTML = '<iframe style="position:fixed;top:0px;left:0px;width:100%;height:100%;z-index:-1;" width="560" height="315" src="https://www.youtube.com/embed/yys5iioLUNw?autoplay=1&showinfo=0&rel=0" frameborder="0" allowfullscreen></iframe>';
 }
+
+function launchGoldfinger(){
+  console.log("SHE DEEEAD!!!");
+  document.getElementById('picture').innerHTML = '<iframe style="position:fixed;top:0px;left:0px;width:100%;height:100%;z-index:-1;" width="560" height="315" src="https://www.youtube.com/embed/Q56PHHlzizs?autoplay=1&showinfo=0&rel=0" frameborder="0" allowfullscreen></iframe>';
+}
+
+
 
 function logFailure(){
   console.log("FAILURE");
@@ -78,6 +86,9 @@ function updateNoteArray(pitch){
   };
   if (relativeNoteArray.toString() == imperial.toString()){
     launchImperial();
+  };
+  if (relativeNoteArray.toString() == goldfinger.toString()){
+    launchGoldfinger();
   };
   
 }
