fab7f6900f469a665f50bb594954fd4b751e89f3
Christopher Alcock <christopheralcock@Christophers-MacBook-Pro-2.local>
2016-03-07 22:37:29 +0000

    started clearing global object with nice namespacing

--- public/piano-keys.js
+++ public/piano-keys.js
@@ -9,24 +9,24 @@ document.onkeydown = onkeyup = function(e){
     keys = keyTranslator(keySet);
     notes = noteTranslator(notes);
 
-    if(keys.a){playSine(0, octaveChooser(notes.c, 3), .1)}
-    if(keys.w){playSine(0, octaveChooser(sharpen(notes.c), 3), 0.1)}
-    if(keys.s){playSine(0, octaveChooser(notes.d, 3), 0.1)}
-    if(keys.e){playSine(0, octaveChooser(sharpen(notes.d), 3), 0.1)}
-    if(keys.d){playSine(0, octaveChooser(notes.e, 3), 0.1)}
-    if(keys.f){playSine(0, octaveChooser(notes.f, 3), 0.1)}
-    if(keys.t){playSine(0, octaveChooser(sharpen(notes.f), 3), 0.1)}
-    if(keys.g){playSine(0, octaveChooser(notes.g, 3), 0.1)}
-    if(keys.y){playSine(0, octaveChooser(sharpen(notes.g), 3), 0.1)}
-    if(keys.h){playSine(0, octaveChooser(notes.a, 3), 0.1)}
-    if(keys.u){playSine(0, octaveChooser(sharpen(notes.a), 3), 0.1)}
-    if(keys.j){playSine(0, octaveChooser(notes.b, 3), 0.1)}
-    if(keys.k){playSine(0, octaveChooser(notes.c, 4), 0.1)}
-    if(keys.o){playSine(0, octaveChooser(sharpen(notes.c), 4), 0.1)}
-    if(keys.l){playSine(0, octaveChooser(notes.d, 4), 0.1)}
-    if(keys.p){playSine(0, octaveChooser(sharpen(notes.d), 4), 0.1)}
-    if(keys.semiColon){playSine(0, octaveChooser(notes.e, 4), 0.1)}
-    if(keys.singleQuote){playSine(0, octaveChooser(notes.f, 4), 0.1)}
-    if(keys.closeBraket){playSine(0, octaveChooser(sharpen(notes.f), 4), 0.1)}
-    if(keys.backSlash){playSine(0, octaveChooser(notes.g, 4), 0.1)}
+    if(keys.a){musicApps.playSine(0, octaveChooser(notes.c, 3), .1)}
+    if(keys.w){musicApps.playSine(0, octaveChooser(sharpen(notes.c), 3), 0.1)}
+    if(keys.s){musicApps.playSine(0, octaveChooser(notes.d, 3), 0.1)}
+    if(keys.e){musicApps.playSine(0, octaveChooser(sharpen(notes.d), 3), 0.1)}
+    if(keys.d){musicApps.playSine(0, octaveChooser(notes.e, 3), 0.1)}
+    if(keys.f){musicApps.playSine(0, octaveChooser(notes.f, 3), 0.1)}
+    if(keys.t){musicApps.playSine(0, octaveChooser(sharpen(notes.f), 3), 0.1)}
+    if(keys.g){musicApps.playSine(0, octaveChooser(notes.g, 3), 0.1)}
+    if(keys.y){musicApps.playSine(0, octaveChooser(sharpen(notes.g), 3), 0.1)}
+    if(keys.h){musicApps.playSine(0, octaveChooser(notes.a, 3), 0.1)}
+    if(keys.u){musicApps.playSine(0, octaveChooser(sharpen(notes.a), 3), 0.1)}
+    if(keys.j){musicApps.playSine(0, octaveChooser(notes.b, 3), 0.1)}
+    if(keys.k){musicApps.playSine(0, octaveChooser(notes.c, 4), 0.1)}
+    if(keys.o){musicApps.playSine(0, octaveChooser(sharpen(notes.c), 4), 0.1)}
+    if(keys.l){musicApps.playSine(0, octaveChooser(notes.d, 4), 0.1)}
+    if(keys.p){musicApps.playSine(0, octaveChooser(sharpen(notes.d), 4), 0.1)}
+    if(keys.semiColon){musicApps.playSine(0, octaveChooser(notes.e, 4), 0.1)}
+    if(keys.singleQuote){musicApps.playSine(0, octaveChooser(notes.f, 4), 0.1)}
+    if(keys.closeBraket){musicApps.playSine(0, octaveChooser(sharpen(notes.f), 4), 0.1)}
+    if(keys.backSlash){musicApps.playSine(0, octaveChooser(notes.g, 4), 0.1)}
 }
--- public/play-sine.js
+++ public/play-sine.js
@@ -1,42 +1,45 @@
-function playSine(delay, pitch, duration) {
-  var startTime = audioContext.currentTime + delay;
-  var endTime = startTime + duration;
+var musicApps = {
 
-  var envelope = audioContext.createGain();
-  envelope.connect(audioContext.destination);
-  envelope.gain.value = 0;
-  envelope.gain.setTargetAtTime(1, startTime, 0.1);
-  envelope.gain.setTargetAtTime(0, endTime, 0.2);
+  playSine: function(delay, pitch, duration) {
+    var startTime = audioContext.currentTime + delay;
+    var endTime = startTime + duration;
 
-  var oscillator1 = audioContext.createOscillator();
-  oscillator1.connect(envelope);
-  oscillator1.type = 'sine';
-  oscillator1.detune.value = (pitch) * 100;
-  oscillator1.start(startTime);
-  oscillator1.stop(endTime + 2);
-}
+    var envelope = audioContext.createGain();
+    envelope.connect(audioContext.destination);
+    envelope.gain.value = 0;
+    envelope.gain.setTargetAtTime(1, startTime, 0.1);
+    envelope.gain.setTargetAtTime(0, endTime, 0.2);
 
-function playSineWithBassOctave(delay, pitch, duration) {
-  var startTime = audioContext.currentTime + delay;
-  var endTime = startTime + duration;
+    var oscillator1 = audioContext.createOscillator();
+    oscillator1.connect(envelope);
+    oscillator1.type = 'sine';
+    oscillator1.detune.value = (pitch) * 100;
+    oscillator1.start(startTime);
+    oscillator1.stop(endTime + 2);
+  },
 
-  var envelope = audioContext.createGain();
-  envelope.connect(audioContext.destination);
-  envelope.gain.value = 0;
-  envelope.gain.setTargetAtTime(1, startTime, 0.1);
-  envelope.gain.setTargetAtTime(0, endTime, 0.2);
+  playSineWithBassOctave: function(delay, pitch, duration) {
+    var startTime = audioContext.currentTime + delay;
+    var endTime = startTime + duration;
 
-  var oscillator1 = audioContext.createOscillator();
-  oscillator1.connect(envelope);
-  oscillator1.type = 'sine';
-  oscillator1.detune.value = (pitch) * 100;
-  oscillator1.start(startTime);
-  oscillator1.stop(endTime + 2);
+    var envelope = audioContext.createGain();
+    envelope.connect(audioContext.destination);
+    envelope.gain.value = 0;
+    envelope.gain.setTargetAtTime(1, startTime, 0.1);
+    envelope.gain.setTargetAtTime(0, endTime, 0.2);
 
-  var oscillator2 = audioContext.createOscillator();
-  oscillator2.connect(envelope);
-  oscillator2.type = 'sine';
-  oscillator2.detune.value = (pitch -12) * 100;
-  oscillator2.start(startTime);
-  oscillator2.stop(endTime + 2);
+    var oscillator1 = audioContext.createOscillator();
+    oscillator1.connect(envelope);
+    oscillator1.type = 'sine';
+    oscillator1.detune.value = (pitch) * 100;
+    oscillator1.start(startTime);
+    oscillator1.stop(endTime + 2);
+
+    var oscillator2 = audioContext.createOscillator();
+    oscillator2.connect(envelope);
+    oscillator2.type = 'sine';
+    oscillator2.detune.value = (pitch -12) * 100;
+    oscillator2.start(startTime);
+    oscillator2.stop(endTime + 2);
+  }
 };
