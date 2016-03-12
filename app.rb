8d4e67ac2ddd0fd813664a2578fe2483ec21ea68
Christopher Alcock <christopheralcock@Christophers-MacBook-Pro-2.local>
2016-03-12 11:16:20 +0000

    allocates different times as well as different pitches

--- app.rb
+++ app.rb
@@ -30,7 +30,8 @@ class SimplePiano < Sinatra::Base
   end
 
   get '/symph' do
-    @ruby_pitch = [-15, -11, -8].sample
+    @ruby_pitch = [-15, -11, -8, -3, 1, 5, 9].sample
+    @ruby_length = [10000, 10100, 10200, 10300, 10400].sample
     erb :symphony
   end
 
