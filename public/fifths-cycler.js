0fb007a7878e0044dbb0821437246cbf875b0f0a
Christopher Alcock <christopheralcock@Christophers-MacBook-Pro-2.local>
2016-03-15 18:54:39 +0000

    updated copy and removed fifths function

---
+++ public/fifths-cycler.js
@@ -0,0 +1,14 @@
+function fifthsCycler(number){
+  if (number % 12 == 0){return 0};
+  if (number % 12 == 1){return 7};
+  if (number % 12 == 2){return 2};
+  if (number % 12 == 3){return 9};
+  if (number % 12 == 4){return 4};
+  if (number % 12 == 5){return 11};
+  if (number % 12 == 6){return 6};
+  if (number % 12 == 7){return 1};
+  if (number % 12 == 8){return 8};
+  if (number % 12 == 9){return 3};
+  if (number % 12 == 10){return 10};
+  if (number % 12 == 11){return 5};
+}
